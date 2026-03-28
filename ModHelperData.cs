fa1ece45006bb5526b64f4c7422f747c752f21cc
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-03-28 08:24:37 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,13 +1,13 @@
-namespace BigBloonSabatogeToggle;
+namespace BigBloonSabotageToggle;
 
 public static class ModHelperData
 {
     public const string WorksOnVersion = "53.0";
     public const string Version = "1.0.0";
-    public const string Name = "BigBloonSabatogeToggle";
+    public const string Name = "BigBloonSabotageToggle";
 
-    public const string Description = "A simple mod that has a toggle to enable or disable the big bloon sabatoge monkey knowledge for boss events.";
+    public const string Description = "A simple mod that has a toggle to enable or disable the big bloon sabotage monkey knowledge for boss events.";
 
     public const string RepoOwner = "RedBlooN-YT";
-    public const string RepoName = "BigBloonSabatogeToggle"; 
+    public const string RepoName = "BigBloonSab0tageToggle"; 
 }
